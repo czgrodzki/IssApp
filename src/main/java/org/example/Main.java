5e955778ad3fc268ec5751392b7830915ca51e33
Bartosz Czerwonogrodzki <bczerwonogrodzki@atlassian.com>
2023-12-02 12:25:10 +0100

    Adding the way to get people.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,6 +18,8 @@ import java.util.Scanner;
 public class Main {
 
     private static final String ISS_API_LOCATION = "http://api.open-notify.org/iss-now.json";
+    private static final String ISS_API_PEOPLE = "http://api.open-notify.org/astros.json";
+
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
@@ -26,7 +28,8 @@ public class Main {
 
         do {
             System.out.println("1. Pobierz położenie ISS");
-            System.out.println("2. Zakończ aplikacje");
+            System.out.println("2. Pobierz ludzi na ISS");
+            System.out.println("3. Zakończ aplikacje");
 
             choice = scanner.nextInt();
             scanner.nextLine();
@@ -37,7 +40,7 @@ public class Main {
 
                     //Stworzenie HTTP klienta, request i wysłanie requestu z rządaniem odpowiedzi
                     HttpClient client = HttpClient.newHttpClient();
-                    HttpRequest request =HttpRequest.newBuilder()
+                    HttpRequest request = HttpRequest.newBuilder()
                             .uri(URI.create(ISS_API_LOCATION))
                             .build();
                     final HttpResponse<String> send = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -67,15 +70,44 @@ public class Main {
                         writer.write(line.toString());
                     }
                     break;
+
                 case 2:
+                    HttpClient client1 = HttpClient.newHttpClient();
+                    HttpRequest request1 = HttpRequest.newBuilder()
+                            .uri(URI.create(ISS_API_PEOPLE))
+                            .build();
+
+                    final HttpResponse<String> response1 = client1.send(request1, HttpResponse.BodyHandlers.ofString());
+
+                    ObjectMapper objectMapper1 = new ObjectMapper();
+                    final JsonNode jsonNode1 = objectMapper1.readTree(response1.body());
+                    final int totalNumber = jsonNode1.at("/number").asInt();
+
+                    StringBuilder people = new StringBuilder();
+                    for (JsonNode jsonArrayNode: jsonNode1.at("/people")) {
+                        String name = jsonArrayNode.at("/name").asText();
+                        System.out.println(name);
+                        people.append(name).append(",");
+                    }
+                    people.append(totalNumber).append("\n");
+
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter("iss_people.csv"))) {
+                        writer.write(people.toString());
+                    }
+
+                    System.out.println("Wszystkich osób jest " + totalNumber);
+                    break;
+
+                case 3:
                     System.out.println("Zamkykamy appkę");
+                    break;
 
                 default:
                     System.out.println("Nie ma takiej komendy");
                     break;
             }
 
-        } while (choice != 2);
+        } while (choice != 3);
 
         scanner.close();
     }
