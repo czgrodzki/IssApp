4c0485bc0afe09b6e3c5b9bdabf3f546a6c3bfc5
Bartosz Czerwonogrodzki <bczerwonogrodzki@atlassian.com>
2023-12-02 11:25:29 +0100

    Adding response mapping.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Scanner;
 
 public class Main {
@@ -34,9 +40,23 @@ public class Main {
                             .build();
                     final HttpResponse<String> send = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+                    //Tworzymy sobie mppera, żeby wciągnąć wartość z JSONa, czyli odpowiedzi z zewnętrznego serwisu
+                    ObjectMapper objectMapper = new ObjectMapper();
 
+                    final JsonNode jsonNode = objectMapper.readTree(send.body());
 
-                    System.out.println(send.body());
+                    //Wyciągamy timestamp jako long
+                    long timestamp = jsonNode.at("/timestamp").asLong();
+
+                    //Tworzymy obiekt instant, który będzie nam potrzebny do stworzenia dalej obiektu LocalDataTiem
+                    Instant instant = Instant.ofEpochSecond(timestamp);
+                    LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+
+                    //Wyciągamy szerokość i długość ISS
+                    final double lat = jsonNode.at("/iss_position/latitude").asDouble();
+                    final double lon = jsonNode.at("/iss_position/longitude").asDouble();
+
+                    System.out.println("Dnia " + localDateTime + " ISS " + " jest w miejscu szerokość: " + lat + " długość " + lon);
                     break;
                 case 2:
                     System.out.println("Zamkykamy appkę");
